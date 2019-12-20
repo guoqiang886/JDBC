11813cd7358e3ebf4c80ba83da677677845fd64d
guoqiang886 <13408059396@163.com>
2019-12-20 09:44:18 +0800

    JDBC练习代码,增删改查部分

---
+++ src/com/jdbc/JDBCTest.java
@@ -0,0 +1,107 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * JDBC学习Demo
+ */
+public class JDBCTest {
+	public static void main(String[] args) {
+		Connection c = null;
+		Statement s = null;
+		try {
+			// 初始化驱动类
+			// 貌似新版本jdk现在不需要初始化驱动类
+			// Class.forName("com.mysql.cj.jdbc.Driver");
+			// System.out.println("JDBC驱动加载成功");
+
+			// 与指定数据库建立连接
+			c = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/jdbc_test?serverTimezone=UTC&characterEncoding=UTF-8", 
+					"root",
+					"ygq1813297517");
+
+			// 創建Statement
+			s = c.createStatement();
+			System.out.println("数据库连接成功，获取连接对象：" + s);
+			
+			// 循环执行多条sql语句
+			for (int i = 0; i < 100; i++) {
+				// 增加100个郭襄
+				String sql = "insert into student(id, name, gender, age, math)"+ "values("+ i + ", '郭襄', '女', 16, 87)";
+				s.execute(sql);
+				System.out.println("第" + (i + 1) + "条sql语句执行成功");
+			}
+			System.out.println("------------------------------");
+			
+			// 删除语句
+			String delSql = "delete from student where name = '郭襄'";
+			s.execute(delSql);
+			System.out.println("删除郭襄成功");
+			System.out.println("------------------------------");
+			
+			// 修改语句
+			String modifySql = "update student set age = '27' where name = '穆念慈'";
+			s.execute(modifySql);
+			System.out.println("修改穆念慈年龄成功");
+			System.out.println("------------------------------");
+			
+			// } catch (ClassNotFoundException e) {
+			// e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				s.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				c.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// 这里通过execute方法来执行sql语句
+		String testSql = "insert into student(id, name, gender, age, math) values(886, '东方不败', '男', null, 88)";
+		execute(testSql);
+		System.out.println("调用execute方法执行sql语句成功");
+	}
+	
+	// 该方法以sql语句作为参数，输入sql语句调用该方法即可执行语句
+	public static void execute(String sql) {
+		Connection c = null;
+		Statement s = null;
+		try {
+			c = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/jdbc_test?serverTimezone=UTC&characterEncoding=UTF-8", 
+					"root",
+					"ygq1813297517");
+			
+			s = c.createStatement();
+			
+			s.execute(sql);
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 这里关闭Statement和Connection的时候也有异常需要处理，更加严谨的做法还要判断s和c是否为null
+			try {
+				s.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				c.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
---
+++ src/com/jdbc/JDBCTest2.java
@@ -0,0 +1,92 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * 设计一个方法，进行分页查询
+ */
+public class JDBCTest2 {
+	public static void main(String[] args) {
+		// 为了保证jdbc_test数据库的student表有足够的数据，这里先对student表添加100组数据
+		Connection c = null;
+		Statement s = null;
+		try {
+			c = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/jdbc_test?serverTimezone=UTC&characterEncoding=UTF-8", 
+					"root",
+					"ygq1813297517");
+
+			s = c.createStatement();
+
+			for (int i = 0; i < 100; i++) {
+				String addSql = "insert into student values(" + i + ", '赵敏', '女', 24, 100)";
+				s.execute(addSql);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				s.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				c.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// 测试list分页查询函数
+		list(20, 10);
+	}
+
+	// 分页查询
+	public static void list(int start, int count) {
+		// start表示从第start行数据开始，count表示查询多少条数据
+		// 这里从已经创建好的jdbc_test数据库来查询数据
+		Connection c = null;
+		Statement s = null;
+		try {
+			c = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/jdbc_test?serverTimezone=UTC&characterEncoding=UTF-8",
+					"root",
+					"ygq1813297517");
+
+			s = c.createStatement();
+
+			String selectSql = "select * from student limit " + start + ", " + count;
+			// System.out.println(selectSql);
+			ResultSet rs = s.executeQuery(selectSql);
+
+			// 遍历ResultSet，可以得到查询的结果
+			while (rs.next()) {
+				int id = rs.getInt("id"); // 可以使用字段名
+				String name = rs.getString(2); // 也可以使用字段顺序，注意字段顺序是从1开始的
+				String gender = rs.getString("gender");
+				int age = rs.getInt(4);
+				int math = rs.getInt("math");
+				// 这里是用的printf格式化输出
+				System.out.printf("%d-%s-%s-%d-%d\n", id, name, gender, age, math);
+				// System.out.println(id + "-" + name + "-" + gender + "-" + age + "-" + math);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				s.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				c.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
